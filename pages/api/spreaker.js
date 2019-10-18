d9334cf9e5a794ae9f283ad13324543275a8dafe
Adrian <eufracioadrian@gmail.com>
2019-10-17 21:40:05 -0700

    Change to peliculas

--- pages/api/spreaker.js
+++ pages/api/spreaker.js
@@ -5,7 +5,7 @@ import Parser from 'rss-parser'
 let parser = new Parser();
 
 export default async (req, res) => {
-  let feed = await parser.parseURL('https://www.spreaker.com/show/3672057/episodes/feed');
+  let feed = await parser.parseURL('https://www.spreaker.com/show/4082207/episodes/feed');
   let data = []
   feed.items.forEach(item => {
     data.push(item);
